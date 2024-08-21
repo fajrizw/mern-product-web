144ff7e9183c392813d83a0250249a12ee746436
fajrizw <fzhahiran@gmail.com>
2024-08-21 17:34:00 +0700

    create post function

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -21,8 +21,28 @@ export const getProductById = async(req, res)=>{
     }
 }
 export const saveProduct = (req, res)=>{
-   
-    
+    if(req.files === null) return res.status(400).json({msg:"No File Uploaded"})
+    const name = req.body.title;
+    const desc = req.body.desc;
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    const fileName = file.md5 + ext ;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    const allowedType = ['.png','.jpg','.jpeg']
+
+    if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg:"Invalid Images"})
+    if(fileSize > 5000000) return res.status(422).json({msg:"Image must be less than 5 mb"})
+
+    file.mv(`./public/images/${fileName}`, async(err)=> {
+        if(err) return res.status(500).json({msg: err.message})
+            try {
+        await Product.create({name: name, description: desc, image: fileName, url:url})
+        res.status(201).json({msg:"Product Created Sucessfully"})
+        } catch (error) {
+            console.log(error.message)
+        }
+    })
 }
 export const updateProduct = (req, res)=>{
     
--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,5 @@ app.use(cors())
 app.use(express.json())
 app.use(fileUpload())
 app.use(ProductRoute)
-
+app.use(express.static("public"))
 app.listen(5000, () => console.log('Server Up and Running...'))
