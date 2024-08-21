50e94e70c0534f7a67e8c32c3043ee9cebfb451d
fajrizw <fzhahiran@gmail.com>
2024-08-21 16:43:55 +0700

    set up end point, database and create get function

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,33 @@
+import Product from "../models/ProductModel.js"
+import path from "path"
+export const getProducts = async(req, res)=>{
+    try {
+        const response = await Product.findAll()
+        res.json(response)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+export const getProductById = async(req, res)=>{
+    try {
+        const response = await Product.findOne({
+            where:{
+                id:req.params.id
+            }
+        })
+        res.json(response)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+export const saveProduct = (req, res)=>{
+   
+    
+}
+export const updateProduct = (req, res)=>{
+    
+}
+export const deleteProduct = (req, res)=>{
+    
+}
+
